f653fb7778381f611097e8569d17fda8400b6c08
Ajaykumar M <m1ajay2001@gmail.com>
2022-09-16 17:23:39 +0530

    proderr

--- _middleware.js
+++
@@ -1,8 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-export async function middleware(req, ev) {
-    const { pathname } = req.nextUrl
-    if (pathname == '/callback') {
-        return NextResponse.redirect('/')
-    }
-    return NextResponse.next()
-}
